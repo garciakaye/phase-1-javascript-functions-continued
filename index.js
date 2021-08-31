e7293e07132c60cbab6c1e6d875d96bab4111a75
kaye garcia <kayemgarcia@gmail.com>
2021-08-31 16:35:56 -0500

    commit tree 4b94bde4cdf9b54765dbd6c74e8333d55c2920cf

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (mondayActivity = "go to the office") {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(param1 = '*'){
+    return function(adj = 'special') {
+        return `You are ${param1}${adj}${param1}!`
+    }
+}
+
+
+// wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
